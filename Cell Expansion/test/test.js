f813c162974ab18e610ec2e31e0042dd78969dbc
mattmotoki <mhmotoki@gmail.com>
2017-02-04 14:21:30 -1000

    started line segments

--- Cell Expansion/test/test.js
+++ Cell Expansion/test/test.js
@@ -1,47 +1,174 @@
+var canvas;
+var bg_ctx;
+var ctx;
+var is_up=true;
+var scale = 0.15;
+var corner_points = [
+  {x:200, y:450, theta:1*Math.PI/4, r:100},
+  {x:250, y:500, theta:1*Math.PI/4, r:100},
 
-var canvas = document.querySelector("#test-canvas");
-var ctx = canvas.getContext("2d");
+  {x:450, y:500, theta:3*Math.PI/4, r:100},
+  {x:500, y:450, theta:3*Math.PI/4, r:100},
 
-/* Draw Filled Dots */
-function addPoint(pos, r) {
-  ctx.save();
-  ctx.beginPath();
-  ctx.arc(pos[0], pos[1], r, 0, 2*Math.PI );
-  ctx.fill();
-  ctx.restore();
+  {x:500, y:250, theta:5*Math.PI/4, r:100},
+  {x:450, y:200, theta:5*Math.PI/4, r:100},
+
+  {x:250, y:200, theta:7*Math.PI/4, r:100},
+  {x:200, y:250, theta:7*Math.PI/4, r:100}
+];
+var center_x=350;
+var center_y=350;
+init();
+
+function init() {
+  // background canvas
+  bg_ctx = document.getElementById("static-canvas").getContext("2d");
+  bg_ctx.strokeRect(200, 200, 300, 300);
+  bg_ctx.stroke();
+  drawGrid(bg_ctx);
+  // for (i = 0; i <corner_points.length; i+=1) {
+  //   drawDot(bg_ctx, corner_points[i].x, corner_points[i].y, 5, "blue");
+  // }
+
+  // dynamic canvas
+  canvas = document.getElementById("dynamic-canvas");
+  ctx = canvas.getContext("2d");
+  canvas.addEventListener("mousemove", function(e) {updatePosition(e);}, false);
+  canvas.addEventListener("mouseup",  function() { is_up = true; });
+  canvas.addEventListener("mousedown", function(e) { is_up = false; updatePosition(e); });
+  drawDot(ctx, center_x, center_y, 5, "black", true);
+  drawScoringBean();
+  // create event listener for resizing
+  window.addEventListener("resize", resizeCanvas, false);
 }
 
-// upper right
-ctx.save();
-ctx.translate(110,10);
-drawCorner();
+// utility functions
+function resizeCanvas() {
+  cancelAnimationFrame(requestId);
+  canvas.height  = window.clientWidth;
+  canvas.width  = window.clientHeight;
+  requestId = animateAttractor();
+}
 
+function  getMousePos(event) {
+  var rect = canvas.getBoundingClientRect(); // abs. size of element
+  var scaleX = canvas.width / rect.width; // relationship bitmap vs. element for X
+  var scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for Y
 
-// bottom right
-ctx.translate(25,100);
-ctx.rotate(Math.PI/2);
-drawCorner();
-ctx.restore();
+  return {
+    x: (event.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
+    y: (event.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+  };
+}
 
-// Test Corner
-function drawCorner() {
-  var p0 = [0, 0];
-  var p1 = [25, 0];
-  var p2 = [25, 25];
+function updatePosition(event) {
+  if (is_up) {return;}
+  var coords = getMousePos(event);
+  var tempx = coords.x;
+  var tempy = coords.y;
+  if (tempx<200 || tempx>500 || tempy<200 || tempy>500) { return;}
+  center_x = tempx;
+  center_y = tempy;
 
-  // draw points
-  addPoint(p0, 3);
-  addPoint(p2, 3);
-
-  // draw curve
+  ctx.save();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.beginPath();
-  ctx.moveTo(p0[0], p0[1]);
-  ctx.bezierCurveTo(
-    p0[0], p0[1],
-    p1[0], p1[1],
-    p2[0], p2[1]
-  );
-  ctx.stroke();
+  ctx.moveTo(center_x, center_y);
+  ctx.arc(center_x, center_y, 5, 2*Math.PI, false);
+  drawDot(ctx, center_x, center_y, 5, "black", true);
+  ctx.fill();
+
+  drawScoringBean();
+  ctx.restore();
 
 
+}
+
+function drawGrid(context) {
+  context.save();
+  context.globalAlpha = 1;
+  context.strokeStyle = "#D3D3D3";
+  context.setLineDash([5, 3]);
+  for (i = 0; i < 9; i+=1) {
+    context.fillText( i*100, 1+i*100, 710);
+    context.fillText( i*100, 0, i*100);
+    context.moveTo(i*100, 0);
+    context.lineTo(i*100, 800);
+    context.moveTo(0, i*100);
+    context.lineTo(800, i*100);
+  }
+  context.stroke();
+  context.fill();
+  context.restore();
+}
+
+function drawDot(context, x, y, r, color, show_text) {
+  // set defaults
+  color = color || "black";
+  show_text = show_text || false;
+
+  context.save();
+  context.moveTo(x, y);
+  context.beginPath();
+  context.fillStyle = color;
+  context.arc(x, y, r, 0, 2*Math.PI, false);
+  if (show_text) {
+    context.fillText("(" + Math.round(x) + ", " + Math.round(y) + ")", x+5, y-5);
+  }
+
+  context.fill();
+  context.stroke();
+  context.restore();
+}
+
+function updateControlPoints() {
+  for (i = 0; i <corner_points.length; i+=1) {
+    row = corner_points[i];
+    row.r = scale*Math.sqrt( Math.pow(row.y - center_y, 2) + Math.pow(row.x - center_x, 2) );
+    row.theta = Math.atan2(row.y - center_y, row.x - center_x)-Math.PI/2;
+  }
+}
+
+function drawScoringBean() {
+  var row;
+  var next_row;
+  var last_cpx;
+  var last_cpy;
+  var xpy;
+  var next_cpx;
+  var next_cpy;
+  var n_points = corner_points.length;
+  updateControlPoints();
+  for (i = 0; i <n_points; i++) {
+
+    row = corner_points[i%n_points];
+    next_row = corner_points[(i+1)%n_points];
+
+    // calculate control points
+    last_cpx = -row.r*Math.cos(row.theta) + row.x;
+    last_cpy = -row.r*Math.sin(row.theta) + row.y;
+    cpx = row.r*Math.cos(row.theta) + row.x;
+    cpy = row.r*Math.sin(row.theta) + row.y;
+    next_cpx = -next_row.r*Math.cos(next_row.theta) + next_row.x;
+    next_cpy = -next_row.r*Math.sin(next_row.theta) + next_row.y;
+
+    // plot start and end points and connecting Bezier curve
+    ctx.save();
+    ctx.beginPath();
+    ctx.strokeStyle = "blue";
+    ctx.moveTo(row.x, row.y);
+    ctx.bezierCurveTo(cpx, cpy, next_cpx, next_cpy, next_row.x, next_row.y);
+    ctx.stroke();
+    ctx.restore();
+
+    // plot control points and line
+    // ctx.strokeStyle = "red";
+    // drawDot(ctx, cpx, cpy, 5, "red", true);
+    // drawDot(ctx, next_cpx, next_cpy, 5, "red", true);
+    // ctx.beginPath();
+    // ctx.moveTo(last_cpx, last_cpy);
+    // ctx.lineTo(cpx, cpy);
+    // ctx.stroke();
+    // ctx.restore();
+  }
 }
--- Cell Expansion/test4/test4.js
+++ Cell Expansion/test4/test4.js
@@ -1,6 +1,7 @@
 (function () {
     var n = 3,
         margin = 10,
+        indent = 10,
         board = document.querySelector("#board"),
         ctx = board.getContext("2d"),
         container = document.querySelector("#board-container"),
@@ -31,7 +32,6 @@
             container.clientWidth,
             container.clientHeight
         );
-
         ctx.canvas.height = board_size;
         ctx.canvas.width = board_size;
         redraw();
@@ -40,9 +40,7 @@
 
     function redraw() {
         drawGrid();
-
         drawOutline(bezier_list);
-
     }
 
     function drawOutline(bezier_list) {
@@ -59,14 +57,13 @@
         ctx.restore();
     }
 
-
     function drawGrid() {
         ctx.save();
         ctx.beginPath();
         ctx.strokeStyle = "#D3D3D3";
+        n_px = scale(n);
         for (var i = 0; i < n + 1; i += 1) {
             i_px = scale(i);
-            n_px = scale(n);
             ctx.moveTo(i_px, margin);
             ctx.lineTo(i_px, n_px);
             ctx.moveTo(margin, i_px);
@@ -90,6 +87,36 @@
         return x * (board_size - 2 * margin) / n + margin;
     }
 
+    // line segment functions
+    function mid(ind, pos) {
+        var seg
+        
+        // set 
+        if (pos>0) {seg = [indent, 0, 0.5, 0, 1-indent, 0];}
+        else       {seg = [indent, 0, 0.5, 0, 1-indent, 0];}
+        
+        
+        switch(pos) {
+            case 1: 
+                seg = [indent, 0, 0.5, 0, 1-indent, 0];
+                break;
+            case -1: 
+                seg = [indent, 1, 0.5, 1, 1-indent, 1];
+                break;
+            case n:
+                seg = [1, indent, 1, 0.5, 1, 1-indent];
+                break;                
+            case -n:
+                seg = [0, indent, 0, 0.5, 0, 1-indent];
+                break;                
+        }
+    }
+
+    function arrayRotate(arr, reverse){
+        if(reverse) { arr.unshift(arr.pop()) } 
+        else { arr.push(arr.shift()) }
+        return arr
+    } 
 })();
 
 
